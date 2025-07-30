a4cb1d01b70e9f65df78b91bb5015af1e1a16752
manisha1029 <manisha@snakescript.com>
2025-07-30 13:22:08 +0530

    Added employee app with CBV

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('students/', views.students, name='students'),
+    path('students/<int:pk>/', views.studentDetailView, name='student-detail'),
+    path('employees/', views.Employees.as_view(), name='employees'),
+    path('employees/<int:pk>/', views.EmployeeDetail.as_view(), name='employee-detail'),
 ]
