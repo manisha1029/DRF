a8b77377a4e7180170c13c77ac6bacfc3982bc89
manisha1029 <manisha@snakescript.com>
2025-07-30 10:02:51 +0530

    Add initial code drf practice project

---
+++ api/views.py
@@ -0,0 +1,20 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from students.models import Student
+from .serializers import StudentSerializer
+
+# Create your views here.
+@api_view(['GET', 'POST'])
+def students(request):
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
